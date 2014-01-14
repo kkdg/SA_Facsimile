bac709a65fbd931056907b531390808f20187fd3
Kwag De Gi <tech@skyaperture.com>
2014-01-15 00:50:25 +0900

    initial complete

--- facsimile.php
+++ facsimile.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: SA-Facsimile
-Plugin URI: http://www.skyaperture.com/plugin
+Plugin URI: http://www.skyaperture.com/plugin/facsimile
 Description: Migration DB to Kboard
 Version: 0.1.0
 Author: Dr.DeX
@@ -17,8 +17,7 @@ class SA_Facsimile {
 		require_once "shd/simple_html_dom.php";
 		add_action( 'admin_menu', array( $this, 'init_the_page' ) );	
 		add_action('admin_enqueue_scripts', array( $this, 'sa_facsimile' ) );
-
-// echo "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";        	
+   	
 	}
 
 	protected static $instance;
@@ -42,7 +41,6 @@ class SA_Facsimile {
 
 	    $capability = 'edit_others_posts';
 
-	    // $menu_slug  = 'judge/judge-tool.php';
 	    $menu_slug  = 'sa-facsimile';
 
 	    $function   = array( $this, 'admin_screen' );
@@ -67,14 +65,22 @@ class SA_Facsimile {
 			return;
 		} 		
 		$br = '<p></p>';
-		echo '<form enctype="multipart/form-data" action="" method="post">
-				<label for="file3">ZIP 파일</label>
-				<input type="file" name="file3" id="file3"><br>'.$br.'
 
-				<label for="run">업로드 :</label><span> 데이터를 
+
+		echo '<form enctype="multipart/form-data" action="" method="post">
+				<label for="file3">ZIP 파일 :</label>
+				<input type="file" name="file3" id="file3"><br>'.$br;
+		echo '<label for=board_id>게시판 선택 :</label><select name=board_id>';
+		while( $row = $this->hasNext() ){
+			echo '<option value='.$row->uid.'>'.$row->board_name.'</option>';
+		}
+		echo '</select>';
+		echo $br;
+		echo '<label for="run">업로드 :</label><span> 데이터를 
 				<input type="submit" class="button-primary" name="run" value="업로드" id="run"> </input> 합니다. </span><br>
 				'.$br.'										
 			 ';			
+
 
 		$files = array();
 		if ( isset( $_FILES ) && count( $_FILES ) != 0 ){
@@ -102,7 +108,15 @@ class SA_Facsimile {
 			// HTML Parse;
 			$location = __DIR__ . '/upload/my/';
 			$target = $this->parse_files();
-
+			$board = $_POST['board_id'];
+			$this->search = 1;
+			$this->secret = false;
+			foreach ( $target as $trgt ){
+				$this->title = $trgt[0];
+				$this->content = $trgt[1];
+				$this->board_id = $board;
+				$this->insert_kboard( $trgt, $board );
+			}
 		}
 
 	}
@@ -162,13 +176,13 @@ class SA_Facsimile {
 			$fend = array_pop( $f );
 			if ( ! preg_match( '/.html$/', $fend ) )
 				continue;
-			// preg_replace( "{\\\}", '/', $fend );
+
 			preg_match_all( '/\d/', $fend, $fres );
 			$fres = implode( '', $fres[0] );
 			$f[] = $fres.".html";
 			$file = implode( '/', $f );
 		}
-// print_r($files);
+
 		
 		for ( $i = 0; $i < count( $files ); $i++ ){
 			rename( $files_to_change[$i], $files[$i] );
@@ -182,33 +196,94 @@ class SA_Facsimile {
 	public function parse_files() {
 		$location = __DIR__.'/upload/my/';
 		$files = glob( $location . '*' );
-// print_r($files);		
+	
 		$target = array();
 		foreach ( $files as $file ){
+			$f = explode( '/', $file );
+			$fend = array_pop( $f );
+			if ( ! preg_match( '/.html$/', $fend ) )
+				continue;			
 			$html = file_get_html( $file );
-// print_r($html);
 
 			$table = $html->find('table', 0);
-// echo $text;			
-// print_r($text);
-			$title = $table->find('td', 1);
 
-			$content = $table->find('td', 5);
-			// echo $title;
-			// echo $content;
+			$title = $table->find('td', 1)->plaintext;
+
+			$content = $table->find('td', 5)->plaintext;
 
 			$title = iconv("EUC-KR", "UTF-8", $title );
 			$content = iconv("EUC-KR", "UTF-8", $content );
 			
 			$target[] = array( $title, $content ) ;
 			
-
-			
 		}
 
 		return $target;
 	}
 
+
+	public function insert_kboard( $target, $board ){
+		global $user_ID;
+
+		$userdata = get_userdata($user_ID);
+		
+		$data['board_id'] = $board;
+		$data['member_uid'] = intval($userdata->data->ID);
+		$data['member_display'] = $this->member_display?$this->member_display:$userdata->data->display_name;
+		$data['title'] = $target[0];
+		$data['content'] = $target[1];
+		$data['date'] = date("YmdHis", current_time('timestamp'));
+		$data['view'] = 0;
+		$data['category1'] = '';
+		$data['category2'] = '';
+		$data['secret'] = false;
+		$data['notice'] = '';
+		$data['search'] = $this->search;
+		$data['thumbnail_file'] = '';
+		$data['thumbnail_name'] = '';
+		$data['password'] = $this->password?$this->password:'';
+		
+		foreach($data as $key => $value){
+			$value = addslashes($value);
+			$insert_key[] = "`$key`";
+			$insert_data[] = "'$value'";
+		}
+		mysql_query("INSERT INTO `".KBOARD_DB_PREFIX."kboard_board_content` (".implode(',', $insert_key).") VALUE (".implode(',', $insert_data).")");	
+		$insert_id = mysql_insert_id();
+		if(!$insert_id) list($insert_id) = mysql_fetch_row(mysql_query("SELECT LAST_INSERT_ID()"));
+		
+		$this->insertPost($insert_id, $data['member_uid']);			
+	}
+
+	public function insertPost($content_uid, $member_uid){
+		if($content_uid && $this->search>0 && $this->search<3){
+			$kboard_post = array(
+				'post_author'   => $member_uid,
+				'post_title'    => $this->title,
+				'post_content'  => ($this->secret=='true' || $this->search==2)?'':$this->content,
+				'post_status'   => 'publish',
+				'comment_status'=> 'closed',
+				'ping_status'   => 'closed',
+				'post_name'     => $content_uid,
+				'post_parent'   => $this->board_id,
+				'post_type'     => 'kboard'
+			);
+			wp_insert_post($kboard_post);
+		}
+	}	
+
+	public function getList(){
+		$resource = mysql_query("SELECT COUNT(*) FROM `".KBOARD_DB_PREFIX."kboard_board_setting` WHERE 1");
+		list($this->total) = mysql_fetch_row($resource);
+		$this->resource = mysql_query("SELECT * FROM `".KBOARD_DB_PREFIX."kboard_board_setting` WHERE 1 ORDER BY uid DESC");
+		return $this->resource;
+	}
+
+	public function hasNext(){
+		if(!$this->resource) $this->getList();
+		$this->row = mysql_fetch_object($this->resource);
+		return $this->row;
+	}
 }
 
 SA_Facsimile::instance();
